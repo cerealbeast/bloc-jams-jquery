6942e49396b9db6f7dd2249c48c7d3f7b126d205
Zak McCune <COFFIN@the-coffin.gateway.sonic.net>
2020-01-23 15:31:34 -0800

    checkpoint 15 blocjams issues

---
+++ scripts/album-info.js
@@ -0,0 +1,7 @@
+{
+  $('#album-title').text(album.title);
+  $('img#album-cover-art').attr('src', album.albumArtUrl);
+  $('#album-artist').text(album.artist);
+  $('#album-release-info').text(album.releaseInfo);
+
+}
