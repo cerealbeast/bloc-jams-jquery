f15dd32b2c0437de90a7c35f97d00f7759bd414c
Zak McCune <COFFIN@the-coffin.gateway.sonic.net>
2020-02-03 11:41:53 -0800

    return to the original code

--- scripts/player-bar.js
+++ scripts/player-bar.js
@@ -36,20 +36,8 @@
 
   setInterval( () => {
     if (player.playState !== 'playing') { return; }
-    let minutes = Math.floor(length / 60);
-    let seconds_int = length - minutes * 60;
-    let seconds_str = seconds_int.toString();
-    let seconds = seconds_str.substr(0, 2);
-    let time = minutes + ':' + seconds;
-    return time;
     const currentTime = player.getTime();
     const duration = player.getDuration();
-    let current_hour = parseInt(currentTime / 3600) % 24;
-    let current_minute = parseInt(currentTime / 60) % 60;
-    let current_seconds_long = currentTime % 60;
-    let current_seconds = current_seconds_long.toFixed();
-    let current_time = (current_minute < 10 ? "0" + current_minute : current_minute) + ":" + (current_seconds < 10 ? "0" + current_seconds : current_seconds);
-    return current_time;
     const percent = (currentTime / duration) * 100;
     $('#time-control .current-time').text( currentTime );
     $('#time-control input').val(percent);
